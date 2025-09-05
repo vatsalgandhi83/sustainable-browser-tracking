75530c1b8b791784eccd0d7656b6c3db484ab7e5
Tushar <tushar.singh01@sjsu.edu>
2025-09-05 13:12:58 -0700

    Implemented grey-scale toggle

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./src/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./popup.html", "./*.html"],
   theme: {
     extend: {},
   },
